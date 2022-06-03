61e8b8cf39e6bba8b14a696e7c30df65b03e6368
Nikhil Madne <nikhil.madne@perennialsys.com>
2022-06-03 12:06:23 +0530

    new file added

---
+++ src/main/java/com/jpa/SpringJpaManytoManyApplication.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaManytoManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaManytoManyApplication.class, args);
+    }
+
+}
